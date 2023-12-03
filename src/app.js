d15d8ad4834c63e851a8f7c7807cf9e67aa77376
Martin Soengas <martin.fs@gmail.com>
2023-12-03 16:57:26 -0300

    update packages and minor fixes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import connection from './config/connection.js';
+import seeder from './helpers/seeder.js';
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-
-import seeder from './helpers/seeder.js';
 
 // Router Imports
 import usersRouter from './routes/users.routes.js';
@@ -20,18 +19,18 @@ app.use('/users', usersRouter);
 
 app.use('/', async (_req, res) => {
   res.status(200).send({
-    system: 'Videogames API',
     status: '🟢 System operational',
-    version: '1.0.0',
   });
 });
 
+// DB Seeder
 await seeder();
 
 // Server
 await connection.sync({ force: false });
 
 const port = process.env.PORT || 3000;
+
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server is running on port ${port}`);
 });
